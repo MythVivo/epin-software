eca3ced2d8c253938faf75c7be82c958d8021d7d
salihay <mrsalihay@gmail.com>
2021-05-20 14:29:57 +0300

    Oyunlar sayfası düzenlendi-1

--- resources/views/front/pages/oyunlar.blade.php
+++ resources/views/front/pages/oyunlar.blade.php
@@ -1,15 +1,28 @@
 @extends('front.layouts.app')
 @section('body')
+<section class="game">
     <div class="container">
-        @foreach(\App\Models\Games::whereNull('deleted_at')->where('status', '1')->get() as $u)
+        <div class="row">
+            @foreach(\App\Models\Games::whereNull('deleted_at')->where('status', '1')->get() as $u)
             <?php
             $image = explode(".", $u->image);
             $image = $image[0] . "@2x." . $image[1];
             ?>
-            <img src="{{asset(env('root').env('front').env('games').$image)}}">
-                <a href="{{route('oyun_baslik', $u->link)}}"> {{$u->title}} </a>
-            {!! Str::limit(strip_tags($u->text), $limit = 100, $end = '...') !!}
-        @endforeach
-    </div>
 
+            <div class="col-md-6 col-lg-4">
+                <div class="game-card">
+                    <figure>
+                        <img src="{{asset(env('root').env('front').env('games').$image)}}">
+                    </figure>
+                    <div class="game-card-description">
+                        <a class="btn-inline color-blue title-button" href="{{route('oyun_baslik', $u->link)}}"> {{$u->title}} </a>
+                        <p>{!! Str::limit(strip_tags($u->text), $limit = 100, $end = '...') !!}</p>
+                    </div>
+                </div>
+            </div>
+
+            @endforeach
+        </div>
+    </div>
+</section>
 @endsection
