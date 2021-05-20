617e75322aedc9af2a298f8c252cd4cfcd7da101
salihay <mrsalihay@gmail.com>
2021-05-20 17:33:33 +0300

    oyun detay güncelleme

--- resources/views/front/pages/oyun-detay.blade.php
+++ resources/views/front/pages/oyun-detay.blade.php
@@ -3,8 +3,11 @@
     <?php
     $oyun = \App\Models\Games::where('link', $oyun)->first();
     ?>
+<div class="container">
+<div class="row">
+<div class="col-md-8">
 
-    @foreach(\App\Models\GamesTitles::whereNull('deleted_at')->where('status', '1')->where('game', $oyun->id)->get() as $u)
+@foreach(\App\Models\GamesTitles::whereNull('deleted_at')->where('status', '1')->where('game', $oyun->id)->get() as $u)
         <img src="{{asset(env('root').env('front').env('games_titles').$u->image)}}">
         <a href="#"> {{$u->title}} </a>
         {!! Str::limit(strip_tags($u->text), $limit = 100, $end = '...') !!}
@@ -14,6 +17,13 @@
 
     {{$oyun->title}}
     {!! $oyun->text !!}
+
+</div>
+
+</div>
+
+</div>
+
 
 
 @endsection
