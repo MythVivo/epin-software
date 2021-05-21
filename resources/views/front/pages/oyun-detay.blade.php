6772d87cad1f3966965e95f2db29120db787e6f1
salihay <mrsalihay@gmail.com>
2021-05-21 14:53:35 +0300

    oyun detay

--- resources/views/front/pages/oyun-detay.blade.php
+++ resources/views/front/pages/oyun-detay.blade.php
@@ -6,7 +6,7 @@ $oyun = \App\Models\Games::where('link', $oyun)->first();
 <section class="pt-140">
     <div class="container">
         <div class="row">
-            <div class="col-md-8">
+            <div class="col-md-4">
 
                 <div class="game-image">
 
@@ -16,17 +16,20 @@ $oyun = \App\Models\Games::where('link', $oyun)->first();
 
 
             </div>
-            <div class="col-md-4">
-                <div class="game-aside">
-                    @foreach(\App\Models\GamesTitles::whereNull('deleted_at')->where('status', '1')->where('game',
-                    $oyun->id)->get() as $u)
-
-                    <figure>
-                        <img src="{{asset(env('root').env('front').env('games_titles').$u->image)}}">
-                        <a href="#"> {{$u->title}} </a>
-                        <p>{!! Str::limit(strip_tags($u->text), $limit = 100, $end = '...') !!}</p>
-                    </figure>
-                    @endforeach
+            <div class="col-md-8">
+                <div class="game-items">
+                    <div class="row">
+                        @foreach(\App\Models\GamesTitles::whereNull('deleted_at')->where('status', '1')->where('game',
+                        $oyun->id)->get() as $u)
+                        <div class="col-md-4">
+                            <figure>
+                                <img src="{{asset(env('root').env('front').env('games_titles').$u->image)}}">
+                                <a href="#"> {{$u->title}} </a>
+                                <p>{!! Str::limit(strip_tags($u->text), $limit = 100, $end = '...') !!}</p>
+                            </figure>
+                        </div>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
