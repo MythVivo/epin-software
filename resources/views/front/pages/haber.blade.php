4896c4c48a2aee1f9504326b86677b959702dd58
salihay <mrsalihay@gmail.com>
2021-06-02 22:36:52 +0300

    haber düzenleme

--- resources/views/front/pages/haber.blade.php
+++ resources/views/front/pages/haber.blade.php
@@ -1,13 +1,28 @@
 @extends('front.layouts.app')
 @section('body')
-        <?php
+<section class="game-news-page header-margin pt-100 pb-100">
+    <div class="container">
+        <div class="row">
+            <?php
             $u = \App\Models\News::where('link', $haber)->first();
-        $image = explode(".", $u->image);
-        $image = $image[0] . "@2x." . $image[1];
-        ?>
-        <img src="{{asset(env('root').env('front').env('news').$image)}}" alt="{{$u->title}}">
-        {{$u->title}}
-        {{$u->text_short}}
-        {{$u->text}}
+            $image = explode(".", $u->image);
+            $image = $image[0] . "@2x." . $image[1];
+            ?>
 
+            <div class="col-md-8">
+                <figure>
+                    <span class="news-in-date">1 Ay Önce</span>
+                    <img src="{{asset(env('root').env('front').env('news').$image)}}" alt="{{$u->title}}">
+                    <h1 class="heading-primary"> {{$u->title}}</h1>
+                </figure>
+                <article>
+
+                    <h4 class="short-text"> {{$u->text_short}}</h4>
+                    <hr>
+                    {{$u->text}}
+                </article>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
--- resources/views/front/pages/haberler.blade.php
+++ resources/views/front/pages/haberler.blade.php
@@ -1,13 +1,33 @@
 @extends('front.layouts.app')
 @section('body')
-    @foreach(\App\Models\News::whereNull('deleted_at')->where('lang', getLang())->orderBy('created_at', 'desc')->where('status', '1')->get() as $u)
-        <?php
-        $image = explode(".", $u->image);
-        $image = $image[0] . "@2x." . $image[1];
-        ?>
-        <img src="{{asset(env('root').env('front').env('news').$image)}}" alt="{{$u->title}}">
-        {{$u->title}}
-        {{$u->text_short}}
-        <a href="{{route('haber_detay', $u->link)}}">sdfsdf</a>
-    @endforeach
+<section class="game header-margin pt-100 pb-100">
+    <div class="container">
+        <div class="row">
+
+            @foreach(\App\Models\News::whereNull('deleted_at')->where('lang', getLang())->orderBy('created_at',
+            'desc')->where('status', '1')->get() as $u)
+            <?php
+            $image = explode(".", $u->image);
+            $image = $image[0] . "@2x." . $image[1];
+            ?>
+
+            <div class="col-md-6 col-lg-4">
+                <div class="game-card">
+                    <figure>
+                        <img src="{{asset(env('root').env('front').env('news').$image)}}" alt="{{$u->title}}">
+                    </figure>
+
+                    <div class="game-card-description">
+                        <span class="date">1 gün önce</span>
+                        <a class="btn-inline color-blue title-button" href="{{route('haber_detay', $u->link)}}">
+                            {{$u->title}} </a>
+                        <p>{{$u->text_short}}</p>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+
+        </div>
+    </div>
+</section>
 @endsection
